e7a38a118694e7398f048aa96f11766e642c81b3
ayush0077 <ayush.tamrakar.90@gmail.com>
2026-03-11 00:28:06 +0545

    adding images slide func

---
+++ js/slider.js
@@ -0,0 +1,50 @@
+const slides = document.querySelectorAll(".slide");
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+const slider = document.querySelector(".slider-container");
+
+let index = 0;
+let autoSlide;
+
+function showSlide(i){
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[i].classList.add("active");
+}
+
+function nextSlide(){
+    index++;
+    if(index >= slides.length){
+        index = 0;
+    }
+    showSlide(index);
+}
+
+function prevSlide(){
+    index--;
+    if(index < 0){
+        index = slides.length - 1;
+    }
+    showSlide(index);
+}
+
+next.addEventListener("click", nextSlide);
+prev.addEventListener("click", prevSlide);
+
+/* AUTO SLIDE FUNCTION */
+
+function startSlider(){
+    autoSlide = setInterval(nextSlide, 4000);
+}
+
+function stopSlider(){
+    clearInterval(autoSlide);
+}
+
+/* PAUSE ON HOVER */
+
+slider.addEventListener("mouseenter", stopSlider);
+slider.addEventListener("mouseleave", startSlider);
+
+/* START SLIDER */
+
+startSlider();
