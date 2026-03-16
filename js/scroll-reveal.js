5b0bcf217845d43f4343d5988f6e774e77984131
ayush0077 <ayush.tamrakar.90@gmail.com>
2026-03-16 14:16:46 +0545

    last update before

---
+++ js/scroll-reveal.js
@@ -0,0 +1,25 @@
+function reveal(){
+
+let reveals = document.querySelectorAll(".reveal");
+
+for(let i = 0; i < reveals.length; i++){
+
+let windowHeight = window.innerHeight;
+let elementTop = reveals[i].getBoundingClientRect().top;
+let elementVisible = 120;
+
+if(elementTop < windowHeight - elementVisible){
+reveals[i].classList.add("active");
+}
+}
+}
+
+window.addEventListener("scroll", reveal);
+window.addEventListener("scroll", function(){
+
+let navbar = document.querySelector(".navbar");
+
+if(window.scrollY > 50){
+navbar.classList.add("visible");
+}
+});
